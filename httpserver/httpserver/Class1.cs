c3ebbfaeeaec00a3399dd5d2ee01afbdcb1507e7
Emil <cliffeg1512@gmail.com>
2022-09-30 18:22:26 +0300

    add httpserver

---
+++ httpserver/httpserver/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace httpserver
+{
+    public class HttpServer
+    {
+        public string Port = "8888/google";
+
+        public HttpListener _listener;
+
+        public void Start()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:" + Port + "/");
+            _listener.Start();
+            Console.WriteLine("Ожидание подключений...");
+            Receive();
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            Console.WriteLine("Обработка подключений завершена");
+        }
+
+        private void Receive()
+        {
+            _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            if (_listener.IsListening)
+            {
+                
+                var context = _listener.EndGetContext(result);
+                var request = context.Request;
+
+                // do something with the request
+                Console.WriteLine($"{request.Url}");
+                Console.WriteLine($"{request.HttpMethod} {request.Url}");
+
+                if (request.HasEntityBody)
+                {
+                    var body = request.InputStream;
+                    var encoding = request.ContentEncoding;
+                    var reader = new StreamReader(body, encoding);
+                    if (request.ContentType != null)
+                    {
+                        Console.WriteLine("Client data content type {0}", request.ContentType);
+                    }
+                    Console.WriteLine("Client data content length {0}", request.ContentLength64);
+
+                    Console.WriteLine("Start of data:");
+                    string s = reader.ReadToEnd();
+                    Console.WriteLine(s);
+                    Console.WriteLine("End of data:");
+                    reader.Close();
+                    body.Close();
+                }
+
+                var response = context.Response;
+
+                var exePath = AppDomain.CurrentDomain.BaseDirectory;//path to exe file
+                var path = Path.Combine(exePath, "indexwithstyle.html");
+                var file = new StreamReader(path);
+
+
+                string responseStr = file.ReadToEnd();
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+
+                Receive();
+            }
+        }
+    }
+}
---
+++ httpserver/httpserver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace httpserver
+{
+    class Program
+    {
+        private static bool _keepRunning = true;
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                Program._keepRunning = false;
+            };
+
+            var httpServer = new HttpServer();
+            httpServer.Start();
+
+            while (Program._keepRunning) { }
+
+            //Закрытие происходит на "Ctrl + C" в консоли
+
+            //httpServer.Stop();
+        }
+    }
+}
