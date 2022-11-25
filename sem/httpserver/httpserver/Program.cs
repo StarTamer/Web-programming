ef05184440e9d2afedcd3f78065fe614e1c14fae
Emil <cliffeg1512@gmail.com>
2022-11-25 18:37:18 +0300

    semestr

---
+++ sem/httpserver/httpserver/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace httpserver
+{
+    class Program
+    {
+        private static bool _keepRunning = true;
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                Program._keepRunning = false;
+            };
+
+            var httpServer = new HttpServer();
+            httpServer.Start();
+
+            while (Program._keepRunning) 
+            {
+                Handler(Console.ReadLine()?.ToLower(), httpServer);
+            }
+
+            //Закрытие происходит на "Ctrl + C" в консоли
+
+            //httpServer.Stop();
+        }
+
+        static void Handler(string comand, HttpServer server)
+        {
+            switch (comand)
+            {
+                case "start":
+                    server.Start();
+                    break;
+
+                case "stop":
+                    server.Stop();
+                    break;
+
+                case "restart":
+                    server.Stop();
+                    server.Start();
+                    break;
+
+                case "exit":
+                    _keepRunning = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ sem/httpserver/httpserver/controllers/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using httpserver.attributes;
+
+namespace httpserver.controllers
+{
+    [HttpController("users")]
+    public class Users
+    {
+        [HttpGET("list")]
+        public List<User> getUsers()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User{ ID = 1, Name = "Igor"});
+            return users;
+        }
+
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
